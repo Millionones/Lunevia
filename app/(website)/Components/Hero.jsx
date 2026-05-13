25b8528053e38cd31f63e9109342da8acd437b84
swalihhaneef <swalihsr7l@gmail.com>
2026-05-13 16:48:14 +0530

    home hero updated

--- app/(website)/Components/Hero.jsx
+++ app/(website)/Components/Hero.jsx
@@ -21,12 +21,12 @@ const Hero = () => {
             >
                 <SwiperSlide>
                     <div className='hero-slide'>
-                        <img src="beautiful-view-wooden-huts-ocean-captured-thailand.jpg" alt="" />
+                        <img src="/lunevia_home_hero1.png" alt="" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className='hero-slide'>
-                        <img src="luxury-thai-massage-pavilion.jpg" alt="" />
+                        <img src="/lunevia_home_hero2.png" alt="" />
                     </div>
                 </SwiperSlide>
             </Swiper>
