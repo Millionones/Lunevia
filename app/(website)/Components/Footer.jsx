616c98d7f2dff745d94be367dbd237996bfb3624
swalihhaneef <122586752+swalihhaneef@users.noreply.github.com>
2026-05-25 15:27:19 +0530

    faq capitalised

--- app/(website)/Components/Footer.jsx
+++ app/(website)/Components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
                                 <Link href="">careers</Link>
                             </li> */}
                             <li>
-                                <Link href="/faq">Faq</Link>
+                                <Link href="/faq">FAQ</Link>
                             </li>
                             <li>
                                 <Link href="/terms-and-conditions">Terms & Conditions</Link>
