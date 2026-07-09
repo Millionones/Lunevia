19c0ac5fb2c81828f2c70176ef5a1e05031d763d
swalihhaneef <swalihsr7l@gmail.com>
2026-07-09 21:40:03 +0530

    changes made

--- app/(website)/destinations/[slug]/Hero.jsx
+++ app/(website)/destinations/[slug]/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = ({ data }) => {
             <div className='hero'>
                 {/* <img src="/Banner_Image_Lunevia.webp" alt="" /> */}
                 <img src={data.mainImage} alt="" />
-                <h1>{data.title}</h1>
+                {/* <h1>{data.title}</h1> */}
             </div>
             <div className='hero-support-badge'>
                 <ul>
                     <li><a href="tel:+916238829339" target='_blank'>{phoneSvg}</a></li>
                     <li><a href="mailto:info@lunevia.in" target='_blank'>{mailSvg}</a></li>
-                    <li><a href="" target='_blank'>{locationIcon}</a></li>
+                    <li><a href="https://maps.app.goo.gl/VnXir9Z3hjZdwDPT9" target='_blank'>{locationIcon}</a></li>
                 </ul>
             </div>
         </section>
