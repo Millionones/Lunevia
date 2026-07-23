cac2084c6ef1fc662104fc29b50fa2e9396941ba
swalihhaneef <swalihsr7l@gmail.com>
2026-07-23 22:33:34 +0530

    update in home slider

--- app/(website)/Components/Hero.jsx
+++ app/(website)/Components/Hero.jsx
@@ -29,6 +29,21 @@ const Hero = () => {
                         <img src="/lunevia_home_hero2.jpg" alt="" />
                     </div>
                 </SwiperSlide>
+                <SwiperSlide>
+                    <div className='hero-slide no-after'>
+                        <img src="/client-room-gallery/LBM05356.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='hero-slide no-after'>
+                        <img src="https://djwonpcpwhtovzdkcdbt.supabase.co/storage/v1/object/public/lunevia/gallery/d3416ff7-84fb-461f-a04f-0cb541dbfb13.webp" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='hero-slide no-after'>
+                        <img src="https://djwonpcpwhtovzdkcdbt.supabase.co/storage/v1/object/public/lunevia/gallery/c988153f-ce7b-4058-9c4f-1c3112c7c848.webp" alt="" />
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </section>
     )
